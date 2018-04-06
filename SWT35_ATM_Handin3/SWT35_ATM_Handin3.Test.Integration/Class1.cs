5f9e48f2afb9954adb37e1e4b77655a3d966ec02
Rune <runekeena@gmail.com>
2018-04-06 14:36:25 +0200

    dotCoverXML file added and projects created. All ready for coding

---
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Integration/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWT35_ATM_Handin3.Test.Integration
+{
+    public class Class1
+    {
+    }
+}
