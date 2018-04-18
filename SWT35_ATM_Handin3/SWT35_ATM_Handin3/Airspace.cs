5089ceba856dc078d1ccd9aa02363370fefc230e
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-04-18 19:45:20 +0200

    Airspace done for now

--- SWT35_ATM_Handin3/SWT35_ATM_Handin3/Airspace.cs
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3/Airspace.cs
@@ -8,9 +8,26 @@ namespace SWT35_ATM_Handin3
 {
     public class Airspace
     {
-        public Boundary CoordinateX { get; set; }
-        public Boundary CoordinateY { get; set; }
-        public Boundary Altitude { get; set; }
+        public Boundary LowerBound { get; set; }
+        public Boundary UpperBound { get; set; }
+
+        public Airspace(Boundary lowerBound, Boundary upperBound)
+        {
+            // Check if lower bounds exceeds south-west corner
+            LowerBound = lowerBound ?? new Boundary(10000, 10000, 500);
+
+            // Error checks for lower bound?
+            
+            // Check if upper bounds exceeds north-east corner
+            UpperBound = upperBound ?? new Boundary(90000, 90000, 20000);
+
+            // Error checks for upper bound?
+
+            // Check that lower bound isn't greater than upper bound
+            if((UpperBound.X <= LowerBound.X) || (UpperBound.Y <= LowerBound.Y) ||
+               (UpperBound.Alt <= LowerBound.Alt))
+                throw new ArgumentException();
+        }
     }
 
     public class Boundary
@@ -19,8 +36,11 @@ namespace SWT35_ATM_Handin3
         public int Y { get; set; }
         public int Alt { get; set; }
 
-        public Boundary()
+        public Boundary(int x = 0, int y = 0, int alt = 0)
         {
+            X = x;
+            Y = y;
+            Alt = alt;
         }
     }
 }
