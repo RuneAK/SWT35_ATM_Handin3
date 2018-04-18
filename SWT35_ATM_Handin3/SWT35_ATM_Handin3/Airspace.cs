bdfca2537f446dce74727e70de312700009fb1e9
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-04-18 19:23:59 +0200

    Airspace beginning

---
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3/Airspace.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWT35_ATM_Handin3
+{
+    public class Airspace
+    {
+        public Boundary CoordinateX { get; set; }
+        public Boundary CoordinateY { get; set; }
+        public Boundary Altitude { get; set; }
+    }
+
+    public class Boundary
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Alt { get; set; }
+
+        public Boundary()
+        {
+        }
+    }
+}
