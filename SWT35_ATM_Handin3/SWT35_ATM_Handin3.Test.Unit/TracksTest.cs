a422af16377f270697b81b47df83fcbfba421437
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-04-19 16:00:12 +0200

    Started

--- SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Unit/TracksTest.cs
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Unit/TracksTest.cs
@@ -3,12 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NSubstitute;
 using NUnit.Framework;
+using SWT35_ATM_Handin3.Interfaces;
 
 namespace SWT35_ATM_Handin3.Test.Unit
 {
     [TestFixture()]
     public class TracksTest
     {
+        private ICalculator _calculator;
+        private Tracks _uut;
+        private ITrack _track;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _track = Substitute.For<ITrack>();
+            _calculator = Substitute.For<ICalculator>();
+            _uut = new Tracks(_calculator);
+        }
+
+        [TestCase()]
+        public void Tracks_CanAdd_Track(ITrack track)
+        {
+
+        }
     }
 }
