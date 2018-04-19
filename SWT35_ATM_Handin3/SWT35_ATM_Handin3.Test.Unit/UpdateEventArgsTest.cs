1a83ca6b093a52b11ca9378e076534f3162382b0
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-04-19 15:37:09 +0200

    Outline for new tests

---
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Unit/UpdateEventArgsTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SWT35_ATM_Handin3.Test.Unit
+{
+    [TestFixture()]
+    public class UpdateEventArgsTest
+    {
+    }
+}
