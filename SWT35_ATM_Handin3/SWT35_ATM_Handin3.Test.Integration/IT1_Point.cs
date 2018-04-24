7f56979398da56af6c16d9c55734fc76580e3552
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-04-24 19:31:47 +0200

    Integration Test

---
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Integration/IT1_Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SWT35_ATM_Handin3;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace SWT35_ATM_Handin3.Test.Integration
+{
+    [TestFixture]
+    public class IT1_Point
+    {
+        //Stubs
+
+        //Units under test
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void FirstFunctionToTest()
+        {
+
+        }
+    }
+}
