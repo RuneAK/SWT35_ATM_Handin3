bd6be8a2110775ee006f8501ecfb2358d6698563
poxbabs <nickolai.ernst@hotmail.com>
2018-04-24 14:21:07 +0200

    SeperationEvent Headache

---
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3/SeparationRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SWT35_ATM_Handin3.Interfaces;
+
+namespace SWT35_ATM_Handin3
+{
+    public class SeparationRepository : ISeperationRepository
+    {
+        private List<SeparationEvent> _separations;
+
+        public void SeperationRepository()
+        {
+            _separations = new List<SeparationEvent>();
+        }
+
+        public void AddSeperationEvent(SeparationEvent e)
+        {
+            var tempSepEvent = Get(e.Tag1, e.Tag2);
+            if (tempSepEvent == null)
+                _separations.Add(e);
+        }
+
+        public void DeleteSeperationEvent(SeparationEvent e)
+        {
+            var tempSepEvent = Get(e.Tag1, e.Tag2);
+            if (tempSepEvent == null) return;
+            _separations.Remove(tempSepEvent);
+        }
+
+        public SeparationEvent Get(string tag1, string tag2)
+        {
+            return _separations.Find(i => i.Tag1 == tag1 && i.Tag2 == tag2);
+        }
+    }
+}
--- SWT35_ATM_Handin3/SWT35_ATM_Handin3/Tracker.cs
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3/Tracker.cs
@@ -17,6 +17,7 @@ namespace SWT35_ATM_Handin3
 		private List<SeparationEvent> _separations = new List<SeparationEvent>();
 		private readonly ITrackFactory _trackFactory;
 		private readonly ICalculator _calculator;
+	    private ISeperationRepository _seperationRepository;
 		
 		public event EventHandler<UpdateEventArgs> TracksUpdated;
 		public event EventHandler<SeparationEvent>SeparationsUpdated;
@@ -24,6 +25,7 @@ namespace SWT35_ATM_Handin3
 		public Tracker(ITrackFactory trackFactory, ICalculator calculator, IAirspace airspace)
 		{
 			_airspace = airspace;
+            _seperationRepository = new SeparationRepository();
 			_calculator = calculator;
 			_trackFactory = trackFactory;
 			_flightTracks = new Tracks(_calculator);
@@ -72,30 +74,16 @@ namespace SWT35_ATM_Handin3
 						if (_calculator.CalculateSeperation(trackOne.Position, trackTwo.Position))
 						{
 							var newSeperationEvent = new SeparationEvent(trackOne.Tag, trackTwo.Tag, DateTime.Now);
-
-							if (_separations.Find(i => i.Tag1 == trackOne.Tag && i.Tag2 == trackTwo.Tag) == null && _separations.Find(i => i.Tag2 == trackOne.Tag && i.Tag1 == trackTwo.Tag) == null)
-							{
-								OnSeparationsUpdated(newSeperationEvent);
-								_separations.Add(newSeperationEvent);
-							}
+                            _seperationRepository.AddSeperationEvent(newSeperationEvent);						
+						}
+						else
+						{
+						    _seperationRepository.DeleteSeperationEvent(_seperationRepository.Get(trackOne.Tag, trackTwo.Tag));
 						}
 					}
 				}
 			}
-
-			if (_separations.Any())
-			{
-				foreach (var se in _separations)
-				{
-					var track1 = _flightTracks.FlightTracks.Find(i => i.Tag == se.Tag1);
-					var track2 = _flightTracks.FlightTracks.Find(i => i.Tag == se.Tag2);
-					if (!_calculator.CalculateSeperation(track1.Position, track2.Position))
-					{
-						//remove me from list PLEASE!
-					}
-				}
-			}
-		}
+        }
 
 		private void OnTracksUpdated(UpdateEventArgs args)
 		{
