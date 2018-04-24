7761fde875f492cf323a3f685411993fdea8d0b4
Rune <runekeena@gmail.com>
2018-04-24 20:17:48 +0200

    .

--- SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Integration/IT2_Tracks.cs
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Integration/IT2_Tracks.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SWT35_ATM_Handin3;
+using NSubstitute;
+using NUnit.Framework;
+
 
 namespace SWT35_ATM_Handin3.Test.Integration
 {
 	[TestFixture]
 		public class IT2_Tracks
 		{
-			//Stubs
+		//Stubs
 
-			//Units under test
+		//Units under test
 
-			[SetUp]
+		//Real
+
+		[SetUp]
 			public void SetUp()
 			{
 
