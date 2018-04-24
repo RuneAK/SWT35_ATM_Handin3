690bbb7717172a62b772a34c255b1befcfe22a47
Rune <runekeena@gmail.com>
2018-04-24 22:02:11 +0200

    IT2_Tracker added

--- SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Integration/IT2_Tracker.cs
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Integration/IT2_Tracker.cs
@@ -3,78 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SWT35_ATM_Handin3;
 using NSubstitute;
 using NUnit.Framework;
-using SWT35_ATM_Handin3;
 using SWT35_ATM_Handin3.Interfaces;
 using TransponderReceiver;
 
 namespace SWT35_ATM_Handin3.Test.Integration
 {
-    [TestFixture]
-    public class IT2_Tracker
-    {
-        //Real
-        private ICalculator _calculator;
-        private IAirspace _airspace;
-        private ITrack _testTrack1;
-        private ITrack _testTrack2;
-        private ITracks _tracks;
+	public class IT3_Tracker
+	{
+		//Real
+		private ICalculator _calculator;
+		private ITrackFactory _trackFactory;
 
-        //Stubs
-        private ITransponderReceiver _transponderReceiver;
-        private ITracker _tracker;
-        private ISeperationRepository _seperationRepository;
-        private IDisplay _display;
-        private ILogger _logger;
+		//Stubs
+		private ITransponderReceiver _transponderReceiver;
+		private IDisplay _display;
+		private ILogger _logger;
 
-        //Units under test
-        private ITrackFactory _uut;
+		//Units under test
+		private ITracker _uut;
 
-        [SetUp]
-        public void SetUp()
-        {
-            //Real
-            _testTrack1 = new Track
-            {
-                Tag = "TestTag1",
-                Position = new Point(20000, 20000, 5000),
-                Timestamp = new DateTime(2000, 1, 1, 12, 00, 00, 000)
-            };
 
-            _testTrack2 = new Track
-            {
-                Tag = "TestTag1",
-                Position = new Point(20300, 20400, 5100),
-                Timestamp = new DateTime(2000, 1, 1, 12, 00, 01, 000)
-            };
-            _calculator = new Calculator(300, 5000);
-            _airspace = new Airspace();
-            _tracks = new Tracks(_calculator, _airspace);
+		[SetUp]
+		public void SetUp()
+		{
+			_transponderReceiver = Substitute.For<ITransponderReceiver>();
+			_display = Substitute.For<IDisplay>();
+			_logger = Substitute.For<ILogger>();
+			_calculator = new Calculator(300, 5000);
+			_trackFactory = new TrackFactory(_transponderReceiver);
+			_uut = new Tracker(_trackFactory, _calculator, _display, _logger);
 
-            //Stubs
-            _display = Substitute.For<IDisplay>();
-            _logger = Substitute.For<ILogger>();           
-            _transponderReceiver = Substitute.For<ITransponderReceiver>();
-            _tracker = Substitute.For<ITracker>();
-            //UUT
-            _uut = new TrackFactory(_transponderReceiver);            
-        }
+		}
 
-        [Test]
-        public void TrackFactory_NewTrack_AddCalled()
-        {
-            var transponderdata = new List<string>
-            {
-                "TAG12;43210;54321;12345;20000101235959999"
-            };
-            var args = new RawTransponderDataEventArgs(transponderdata);
+		[Test]
+		public void NewTransponderData_OneTrackInfoWritten()
+		{
+			var testInfo = "TestTag1;20000;20000;5000;20001231235959000";
+			var transpondersdata = new List<string>();
+			transpondersdata.Add(testInfo);
+			var args = new RawTransponderDataEventArgs(transpondersdata);
 
-            //Act
-            _transponderReceiver.TransponderDataReady += Raise.EventWith(args);
+			_transponderReceiver.TransponderDataReady += Raise.EventWith(args);
 
-            //Assert
-            
-        }
-    }
+			_display.Received(1).Write("Tag: TestTag1 CurrentPosition: 20000mE,20000mN Altitude: 5000m HorizontalVelocity: 0m/s CompassCourse: 0°");
+		}
+
+		[Test]
+		public void NewTransponderData_SeparationInfoWritten()
+		{
+			var testInfo1 = "TestTag1;20000;20000;5000;20001231235959000";
+			var testInfo2 = "TestTag2;20000;20000;5000;20001231235959000";
+			var transpondersdata = new List<string>();
+			transpondersdata.Add(testInfo1);
+			transpondersdata.Add(testInfo2);
+			var args = new RawTransponderDataEventArgs(transpondersdata);
+
+			_transponderReceiver.TransponderDataReady += Raise.EventWith(args);
+
+			_display.Received(1).WriteRed("TestTag1/TestTag2 Time: 24-Apr-18 21:43:07");
+		}
+	}
 }
