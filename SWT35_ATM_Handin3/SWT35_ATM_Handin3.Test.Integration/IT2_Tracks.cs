bb9017b924d92bdf3a2d2646f5fe25c01d068c55
Rune <runekeena@gmail.com>
2018-04-24 20:09:29 +0200

    -

---
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Integration/IT2_Tracks.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWT35_ATM_Handin3.Test.Integration
+{
+	[TestFixture]
+		public class IT2_Tracks
+		{
+			//Stubs
+
+			//Units under test
+
+			[SetUp]
+			public void SetUp()
+			{
+
+			}
+
+			[Test]
+			public void FirstFunctionToTest()
+			{
+
+			}
+		}
+}
