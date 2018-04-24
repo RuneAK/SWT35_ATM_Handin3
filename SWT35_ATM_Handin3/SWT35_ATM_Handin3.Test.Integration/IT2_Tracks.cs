43e6591770f315492ee1e0bc8606631d79e6b5a8
Rune <runekeena@gmail.com>
2018-04-24 20:46:07 +0200

    -

--- SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Integration/IT2_Tracks.cs
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Integration/IT2_Tracks.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using SWT35_ATM_Handin3;
 using NSubstitute;
 using NUnit.Framework;
+using SWT35_ATM_Handin3.Interfaces;
 
 
 namespace SWT35_ATM_Handin3.Test.Integration
@@ -13,22 +14,59 @@ namespace SWT35_ATM_Handin3.Test.Integration
 	[TestFixture]
 		public class IT2_Tracks
 		{
-		//Stubs
+			//Real
+			private ICalculator _calculator;
+			private IAirspace _airspace;
+			private ITrack _testTrack1;
+			private ITrack _testTrack2;
 
-		//Units under test
+			//Stubs
+			private IDisplay _display;
+			private ILogger _logger;
+			private ISeperationRepository _seperationRepository;
+			private ITrackFactory _trackFactory;
+			private ITracker _tracker;
 
-		//Real
+			//Units under test
+			private ITracks _uut;
+		
 
-		[SetUp]
+			[SetUp]
 			public void SetUp()
 			{
+				_testTrack1 = new Track
+				{
+					Tag = "TestTag1",
+					Position = new Point(20000, 20000, 5000),
+					Timestamp = new DateTime(2000, 1, 1, 12, 00, 00, 000)
+				};
+
+				_testTrack2 = new Track
+				{
+					Tag = "TestTag1",
+					Position = new Point(20300, 20400, 5100),
+					Timestamp = new DateTime(2000, 1, 1, 12, 00, 01, 000)
+				};
+
+				_calculator = new Calculator(300,5000);
+				_airspace = new Airspace();
+				_uut = new Tracks(_calculator,_airspace);
+
+				_display = Substitute.For<IDisplay>();
+				_logger = Substitute.For<ILogger>();
+				_s
 
 			}
 
 			[Test]
 			public void FirstFunctionToTest()
 			{
-
+				var testTracks = new Tracks();
+				testTracks.Add(_testTrack1);
+				_uut.Update(testTracks);
+				testTracks = new Tracks();
+				testTracks.Add(_testTrack2);
+				_uut.Update(testTracks);
 			}
 		}
 }
