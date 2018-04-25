14afdf0c110c2de83a95071b097c8994ed197e28
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-04-25 07:28:51 +0200

    --

--- SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Unit/SeparationRepositoryTest.cs
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Unit/SeparationRepositoryTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NSubstitute;
+using NSubstitute.Extensions;
 using NUnit.Framework;
 
 namespace SWT35_ATM_Handin3.Test.Unit
@@ -13,13 +14,15 @@ namespace SWT35_ATM_Handin3.Test.Unit
     {
         private SeparationRepository _uut;
         private SeparationEvent _separationEvent;
+        private SeparationEvent _separationEvent2;
 
-	    [SetUp]
+        [SetUp]
 	    public void SetUp()
 	    {
             _uut = new SeparationRepository();
             _separationEvent = new SeparationEvent("Test1", "Test2", new DateTime(1999, 12, 31, 12, 00, 00));
-	    }
+	        _separationEvent2 = new SeparationEvent("Test3", "Test4", new DateTime(2000, 10, 25, 10, 00, 00));
+        }
 
         [Test]
         public void Test_GetSepEvent_ReturnsSeperation()
@@ -51,6 +54,14 @@ namespace SWT35_ATM_Handin3.Test.Unit
             _uut.AddSeperationEvent(_separationEvent);
             _uut.DeleteSeperationEvent(_separationEvent);
             Assert.That(!_uut.GetAll().Contains(_separationEvent) && _uut.GetAll().Count == 0);
+        }
+
+        [Test]
+        public void Test_Delete_DeleteSeparationReturnsNull()
+        {
+            _uut.AddSeperationEvent(_separationEvent);
+            _uut.DeleteSeperationEvent(_separationEvent2);
+            Assert.That(_uut.GetAll().Contains(_separationEvent) && !_uut.GetAll().Contains(_separationEvent2));
         }
     }
 }
