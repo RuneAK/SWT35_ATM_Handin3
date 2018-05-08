b3c9d5cd74ef4196a693ecfb1bc72dc138719bb0
Rune <runekeena@gmail.com>
2018-05-08 14:11:36 +0200

    Filter added

--- SWT35_ATM_Handin3/SWT35_ATM_Handin3/Domain/Filter.cs
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3/Domain/Filter.cs
@@ -10,17 +10,30 @@ namespace SWT35_ATM_Handin3.Domain
 {
 	public class Filter : IFilter
 	{
+		private IAirspace _airspace;
+		public event EventHandler<EventTracks> TracksFiltered;
 		public Filter(IAirspace airspace, IFactory factory)
 		{
+			_airspace = airspace;
 			factory.TracksReady += FilterTracks;
 		}
 
 		private void FilterTracks(object sender, EventTracks e)
 		{
+			var filteredTracks = new List<ITrack>();
+
 			foreach (var track in e.TrackData)
 			{
-				
+				if (_airspace.CalculateWithinAirspace(track.Position))
+					filteredTracks.Add(track);
 			}
+
+			FilteredTrackEvent(new EventTracks(filteredTracks));
+		}
+
+		protected virtual void FilteredTrackEvent(EventTracks e)
+		{
+			TracksFiltered?.Invoke(this, e);
 		}
 	}
 }
--- SWT35_ATM_Handin3/SWT35_ATM_Handin3/Interfaces/IFilter.cs
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3/Interfaces/IFilter.cs
@@ -1,6 +1,10 @@
-﻿namespace SWT35_ATM_Handin3.Domain
+﻿using System;
+using SWT35_ATM_Handin3.Boundary;
+
+namespace SWT35_ATM_Handin3.Domain
 {
 	public interface IFilter
 	{
+		event EventHandler<EventTracks> TracksFiltered;
 	}
 }
