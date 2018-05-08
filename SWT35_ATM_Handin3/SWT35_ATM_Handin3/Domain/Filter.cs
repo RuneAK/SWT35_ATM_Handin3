7da75b39117504e124b22058b9c2ac7de2911b22
Rune <runekeena@gmail.com>
2018-05-08 14:05:26 +0200

    Filter started

---
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3/Domain/Filter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SWT35_ATM_Handin3.Boundary;
+using SWT35_ATM_Handin3.Interfaces;
+
+namespace SWT35_ATM_Handin3.Domain
+{
+	public class Filter : IFilter
+	{
+		public Filter(IAirspace airspace, IFactory factory)
+		{
+			factory.TracksReady += FilterTracks;
+		}
+
+		private void FilterTracks(object sender, EventTracks e)
+		{
+			foreach (var track in e.TrackData)
+			{
+				
+			}
+		}
+	}
+}
