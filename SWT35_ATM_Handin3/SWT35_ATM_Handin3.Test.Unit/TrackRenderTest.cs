0450f8436c02ceb5e21d073b79b0fc62d3c22899
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-05-08 21:58:01 +0200

    Outline for TrackRenderTest added

---
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Unit/TrackRenderTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using SWT35_ATM_Handin3.Domain;
+using SWT35_ATM_Handin3.Interfaces;
+
+namespace SWT35_ATM_Handin3.Test.Unit
+{
+    [TestFixture()]
+    public class TrackRenderTest
+    {
+        //Stubs
+        private IDisplay _display;
+        private IUpdate _update;
+        //Fields
+
+        //Unit under test
+        private TrackRender _uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            //Stubs
+            _display = Substitute.For<IDisplay>();
+            _update = Substitute.For<IUpdate>();
+            //Fields
+
+            //Unit under test
+            _uut = new TrackRender(_update, _display);
+        }
+
+        [Test]
+        private void RenderTracks_ATrack_DisplayFunctionsCalled()
+        {
+
+        }
+    }
+}
