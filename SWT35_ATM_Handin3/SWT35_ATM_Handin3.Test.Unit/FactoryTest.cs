8fd523e80f35aa82e58f3e2f1232d408f7b152ec
poxbabs <nickolai.ernst@hotmail.com>
2018-05-08 13:50:49 +0200

    First test added

---
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Unit/FactoryTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using SWT35_ATM_Handin3.Boundary;
+using SWT35_ATM_Handin3.Interfaces;
+using TransponderReceiver;
+
+namespace SWT35_ATM_Handin3.Test.Unit
+{
+    [TestFixture()]
+    public class FactoryTest
+    {
+        private ITransponderReceiver _transponderReceiver;
+        private Factory _uut;
+        private List<ITrack> _trackList;
+        private int _nEventsReceived;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _transponderReceiver = Substitute.For<ITransponderReceiver>();
+            _uut = new Factory(_transponderReceiver);
+
+            _uut.TracksReady += (o, args) =>
+            {
+                _trackList = args.TrackData;
+                ++_nEventsReceived;
+            };
+        }
+
+        [TestCase("TAG12;43210;54321;12345;20000101235959999")]
+        public void CreateTracks_NewTrackAdded_ListContainsTrack(string info)
+        {
+            //Arrange
+            var transponderData = new List<string>();
+            transponderData.Add(info);
+            var args = new RawTransponderDataEventArgs(transponderData);
+
+            //Act
+            _transponderReceiver.TransponderDataReady += Raise.EventWith(args);
+
+            //Assert
+            Assert.That(_trackList[0].Tag, Is.EqualTo(info.Split(';')[0]));
+            Assert.That(_trackList[0].Position.X, Is.EqualTo(Int32.Parse(info.Split(';')[1])));
+            Assert.That(_trackList[0].Position.Y, Is.EqualTo(Int32.Parse(info.Split(';')[2])));
+            Assert.That(_trackList[0].Position.Altitude, Is.EqualTo(Int32.Parse(info.Split(';')[3])));
+            Assert.That(_trackList[0].Timestamp,
+                Is.EqualTo(DateTime.ParseExact(info.Split(';')[4], "yyyyMMddHHmmssfff",
+                    null)));
+            Assert.That(_nEventsReceived, Is.EqualTo(1));
+        }
+    }
+}
--- SWT35_ATM_Handin3/SWT35_ATM_Handin3/Interfaces/ITrack.cs
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3/Interfaces/ITrack.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SWT35_ATM_Handin3.Domain;
 
 namespace SWT35_ATM_Handin3.Interfaces
 {
 	public interface ITrack
 	{
-	}
+	    string Tag { get; set; }
+	    Point Position { get; set; }
+	    DateTime Timestamp { get; set; }
+	    double Velocity { get; set; }
+	    double Course { get; set; }
+    }
 }
