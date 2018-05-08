835cbb1b9ed67c090a25b5ca920bac95a35fd6e4
Rune <runekeena@gmail.com>
2018-05-08 13:03:04 +0200

    Cleaned

--- SWT35_ATM_Handin3/SWT35_ATM_Handin3.Application/Program.cs
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3.Application/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SWT35_ATM_Handin3.Interfaces;
 using TransponderReceiver;
 
 namespace SWT35_ATM_Handin3.Application
@@ -12,14 +11,7 @@ namespace SWT35_ATM_Handin3.Application
 	{
 		static void Main(string[] args)
 		{
-			ITransponderReceiver transponderReceiver = TransponderReceiverFactory.CreateTransponderDataReceiver();
-			ITrackFactory trackFactory = new TrackFactory(transponderReceiver);
-			ICalculator calculator = new Calculator(300,5000);
-			IDisplay display = new Display();
-			ILogger logger = new Logger();
-			ITracker tracker = new Tracker(trackFactory, calculator, display, logger);
-
-			Console.ReadLine();
+			
 		}
 	}
 }
--- SWT35_ATM_Handin3/SWT35_ATM_Handin3/Interfaces/ILogger.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace SWT35_ATM_Handin3.Interfaces
-{
-	public interface ILogger
-	{
-		void WriteToFile(string output);
-	}
-}
