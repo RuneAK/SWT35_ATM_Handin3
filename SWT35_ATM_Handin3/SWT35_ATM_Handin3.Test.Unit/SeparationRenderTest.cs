9c6fd9ab49edebabc99dda2772ce4851f1fa5165
Rune <runekeena@gmail.com>
2018-05-09 09:19:26 +0200

    -

--- SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Unit/SeparationRenderTest.cs
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Unit/SeparationRenderTest.cs
@@ -17,42 +17,43 @@ namespace SWT35_ATM_Handin3.Test.Unit
         //Stubs
         private IDisplay _display;
         private ISeparationDetector _separationDetector;
+		//Fields
+		private Point _pointOne;
+	    private Point _pointTwo;
+		//Unit under test
+		private SeparationRender _uut;
 
-        //Fields
-
-        //Unit under test
-        private SeparationRender _uut;
         [SetUp]
         public void SetUp()
         {
             //Stubs
             _display = Substitute.For<IDisplay>();
             _separationDetector = Substitute.For<ISeparationDetector>();
-            //Fields
-
-            //Unit under test
-            _uut = new SeparationRender(_separationDetector, _display);
+			//Fields
+			
+			//Unit under test
+			_uut = new SeparationRender(_separationDetector, _display);
         }
+		/*
+		[Test]
+		public void RenderSeparations_ASeparation_DisplayFunctionsCalled()
+		{
+			//Arrange
+			var separationList = new List<ISeparation>();
+			var separationOne = new Separation("tag1", "tag2", new DateTime(2000, 1, 1, 1, 1, 1));
+			var separationTwo = new Separation("tag3", "tag4", new DateTime(2000, 1, 1, 1, 1, 1));
+			separationList.Add(separationOne);
+			separationList.Add(separationTwo);
+			var args= new EventSeparation(separationList);
 
-        //[TestCase("one", "two", "three", "four")]
-        //public void RenderSeparations_ASeparation_DisplayFunctionsCalled(string tag1, string tag2, string tag3, string tag4)
-        //{
-        //    //Arrange
-        //    DateTime testTime = new DateTime(2000, 1, 1, 1, 1, 1);
-        //    var separationOne = new Separation(tag1, tag2, testTime);
-        //    var separationTwo = new Separation(tag3, tag4, testTime);
-        //    var separationList = new List<ISeparation>();
-        //    separationList.Add(separationOne);
-        //    separationList.Add(separationTwo);
-        //    var args = new EventSeparation(separationList);
+			//Act
+			_separationDetector.SeparationsUpdated += Raise.EventWith(args);
 
-        //    //Act
-        //    _separationDetector.SeparationsUpdated += Raise.EventWith(args);
-
-        //    //Assert
-        //    _display.Received(1).Write("***Separations***");
-        //    _display.Received(1).Write($"Tag1: " + separationOne.Tag1 + " Tag2: " + separationOne.Tag2 + separationOne.TimeStamp);
-        //    _display.Received(1).Write($"Tag1: " + separationTwo.Tag1 + " Tag2: " + separationTwo.Tag2 + separationTwo.TimeStamp);
-        //}
-    }
+			//Assert
+			_display.Received(1).Write("***Separations***");
+			_display.Received(1).Write($"Tag1: " + separationOne.Tag1 + " Tag2: " + separationOne.Tag2 + separationOne.TimeStamp);
+			_display.Received(1).Write($"Tag1: " + separationTwo.Tag1 + " Tag2: " + separationTwo.Tag2 + separationTwo.TimeStamp);
+		}
+		*/
+	}
 }
