1789707e087b8189f087ab9bdf1e755ec72fa0b7
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-05-09 08:29:10 +0200

    TrackRender tests added, SeparationDetector tests added

--- SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Unit/SeparationDetectorTest.cs
+++ SWT35_ATM_Handin3/SWT35_ATM_Handin3.Test.Unit/SeparationDetectorTest.cs
@@ -42,16 +42,48 @@ namespace SWT35_ATM_Handin3.Test.Unit
             };
         }
 
-        [TestCase()]
-        public void DetectSeparation_TwoTracksCausingSeparation_LoggerUsed()
+        [TestCase(10000, 10000, 10000, 10005, 10005, 10005, "one", "two")]
+        public void DetectSeparation_TwoTracksCausingSeparation_CalculateSeparationTrueSeparationsAdded(int x1, int y1, int alt1, int x2, int y2, int alt2, string tag1, string tag2)
         {
+            //Arrange
+            Point pointOne = new Point(x1, y1, alt1);
+            Point pointTwo = new Point(x2, y2, alt2);
+            var trackOne = new Track(tag1, pointOne, new DateTime(2000, 1, 1, 1, 1, 1));
+            var trackTwo = new Track(tag2, pointTwo, new DateTime(2000, 1, 1, 1, 1, 1));
+            var trackList = new List<ITrack>();
+            trackList.Add(trackOne);
+            trackList.Add(trackTwo);
+            var args = new EventTracks(trackList);
 
+            //Act
+            _update.TracksUpdated += Raise.EventWith(args);
+
+            //Assert
+            Assert.That(_separationData.Count, Is.EqualTo(2));
+            Assert.That(_separationData[0].Tag1, Is.EqualTo(tag1));
+            Assert.That(_separationData[0].Tag2, Is.EqualTo(tag2));
+            Assert.That(_separationData[1].Tag1, Is.EqualTo(tag2));
+            Assert.That(_separationData[1].Tag2, Is.EqualTo(tag1));
         }
 
-        [TestCase()]
-        public void DetectSeparation_TwoTracksCausingNoSeparation_CalculateSeparationFalse()
+        [TestCase(10000, 10000, 10000, 20000, 20000, 20000, "one", "two")]
+        public void DetectSeparation_TwoTracksCausingNoSeparation_CalculateSeparationFalse(int x1, int y1, int alt1, int x2, int y2, int alt2, string tag1, string tag2)
         {
+            //Arrange
+            Point pointOne = new Point(x1, y1, alt1);
+            Point pointTwo = new Point(x2, y2, alt2);
+            var trackOne = new Track(tag1, pointOne, new DateTime(2000, 1, 1, 1, 1, 1));
+            var trackTwo = new Track(tag2, pointTwo, new DateTime(2000, 1, 1, 1, 1, 1));
+            var trackList = new List<ITrack>();
+            trackList.Add(trackOne);
+            trackList.Add(trackTwo);
+            var args = new EventTracks(trackList);
 
+            //Act
+            _update.TracksUpdated += Raise.EventWith(args);
+
+            //Assert
+            Assert.That(_separationData.Count, Is.EqualTo(0));
         }
 
         [TestCase()]
